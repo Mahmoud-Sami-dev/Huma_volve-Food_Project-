fa1e0b6457e3fefcf2d80cf29f7db0cce6ff6e43
Mahmoud_Sami <mahmoudsaminassf@gmail.com>
2026-09-11 20:07:01 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const mealRoutes = require('./routes/mealRoutes');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
@@ -22,6 +23,7 @@ app.get('/', (req, res) => {
 
 // Mount module routes
 app.use('/api/auth', authRoutes);
+app.use('/api', mealRoutes);
 
 // Catch-all 404 handler
 app.use((req, res) => {
